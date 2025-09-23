7d1abae8b104f56f8ea7a2ecedf058a92ee381fb
Daniel Guerra-Rojas <danielguerra135@gmail.com>
2025-09-23 16:10:14 -0700

    Setup FreeRTOS to kick off processes to have two LEDs alternate between different states of brightness

--- IoT-Breathalyzer-Code/src/main.c
+++ IoT-Breathalyzer-Code/src/main.c
@@ -1,25 +1,61 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
-
 #include <stdio.h>
+
+//freertos headers
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+//GPIO header
 #include "driver/gpio.h"
+
+//LED header
 #include "driver/ledc.h"
+
+//ADC API headers
+#include "esp_adc/adc_oneshot.h"
+
+//Wifi header
 #include "esp_wifi.h"
 
-#define BUZZER      GPIO_NUM_32
-#define RED_LED     GPIO_NUM_33
-#define GREEN_LED   GPIO_NUM_25
+#define BUZZER_PIN      GPIO_NUM_32
+#define RED_LED_PIN     GPIO_NUM_33
+#define GREEN_LED_PIN   GPIO_NUM_25
 
-#define LEDC_MODE LEDC_LOW_SPEED_MODE
-#define LEDC_TIMER LEDC_TIMER_0
-#define LEDC_CHANNEL LEDC_CHANNEL_0
+#define LEDC_MODE           LEDC_LOW_SPEED_MODE
+#define LEDC_TIMER          LEDC_TIMER_0
+#define RED_LEDC_CHANNEL    LEDC_CHANNEL_0
+#define GREEN_LEDC_CHANNEL  LEDC_CHANNEL_1
 
+void redLightTask(){
+    while(true){
+        ledc_set_duty(LEDC_MODE, RED_LEDC_CHANNEL, 255);  //on at full brightness
+        ledc_update_duty(LEDC_MODE, RED_LEDC_CHANNEL);
+        vTaskDelay(pdMS_TO_TICKS(2000));
 
+        ledc_set_duty(LEDC_MODE, RED_LEDC_CHANNEL, 255/2);  //on at half brightness
+        ledc_update_duty(LEDC_MODE, RED_LEDC_CHANNEL);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        ledc_set_duty(LEDC_MODE, RED_LEDC_CHANNEL, 0);  //off
+        ledc_update_duty(LEDC_MODE, RED_LEDC_CHANNEL);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+void greenLightTask(){
+    while(true){
+        ledc_set_duty(LEDC_MODE, GREEN_LEDC_CHANNEL, 0);  //off
+        ledc_update_duty(LEDC_MODE, GREEN_LEDC_CHANNEL);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        ledc_set_duty(LEDC_MODE, GREEN_LEDC_CHANNEL, 255/2);  //on at half brightness
+        ledc_update_duty(LEDC_MODE, GREEN_LEDC_CHANNEL);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        ledc_set_duty(LEDC_MODE, GREEN_LEDC_CHANNEL, 255);  //on at full brightness
+        ledc_update_duty(LEDC_MODE, GREEN_LEDC_CHANNEL);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
 
 void app_main(void)
 {
@@ -27,13 +63,12 @@ void app_main(void)
     const char* ssid = "SSID_HERE";
     const char* password = "PASSWORD_HERE";
 
-    gpio_set_direction(BUZZER, GPIO_MODE_OUTPUT);
-    gpio_set_direction(RED_LED, GPIO_MODE_OUTPUT);
-    gpio_set_direction(GREEN_LED, GPIO_MODE_OUTPUT);
+    gpio_set_direction(BUZZER_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(RED_LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GREEN_LED_PIN, GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_INPUT);
     gpio_set_direction(GPIO_NUM_12, GPIO_MODE_INPUT);
 
-    const int ledChannel = 0;
     // Configure LEDC timer
     ledc_timer_config_t ledc_timer = {
         .speed_mode = LEDC_MODE,
@@ -43,27 +78,27 @@ void app_main(void)
     };
     ledc_timer_config(&ledc_timer);
 
-    // Configure LEDC channel
-    ledc_channel_config_t ledc_channel = {
-        .gpio_num = RED_LED,
+    // Configure Red LEDC channel
+    ledc_channel_config_t red_ledc_channel = {
+        .gpio_num = RED_LED_PIN,
         .speed_mode = LEDC_MODE,
-        .channel = LEDC_CHANNEL,
+        .channel = RED_LEDC_CHANNEL,
         .timer_sel = LEDC_TIMER,
         .duty = 0
     };
-    ledc_channel_config(&ledc_channel);
+    ledc_channel_config(&red_ledc_channel);
 
-    while(true){
-        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 255);  //on at full brightness
-        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-        vTaskDelay(pdMS_TO_TICKS(2000));
+    // Configure Green LEDC channel
+    ledc_channel_config_t green_ledc_channel = {
+        .gpio_num = GREEN_LED_PIN,
+        .speed_mode = LEDC_MODE,
+        .channel = GREEN_LEDC_CHANNEL,
+        .timer_sel = LEDC_TIMER,
+        .duty = 0
+    };
+    ledc_channel_config(&green_ledc_channel);
 
-        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 255/2);  //on at half brightness
-        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-        vTaskDelay(pdMS_TO_TICKS(2000));
-
-        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0);  //off
-        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
+    //creating tasks that should run until power off/reset
+    xTaskCreate(redLightTask, "redLightTask function", 500, NULL, 1, NULL);
+    xTaskCreate(greenLightTask, "greenLightTask function", 500, NULL, 1, NULL);
 }
