27ff827186e436edfc68a63d444aae58510e2d99
Daniel Guerra-Rojas <danielguerra135@gmail.com>
2025-10-03 14:52:14 -0700

    Added functionality to read from Alcohol sensor and use outside of running task/Helper Function

--- IoT-Breathalyzer-Code/src/main.c
+++ IoT-Breathalyzer-Code/src/main.c
@@ -57,6 +57,32 @@ void greenLightTask(){
     }
 }
 
+void alcoholSensorTask(void* data){
+    int* out_raw_GPIO2 = (int*) data;
+
+    //Creating ADC Unit Handle
+    adc_oneshot_unit_handle_t adc2_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_2,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc2_handle));
+
+    //Creating ADC Channel to setup input pins
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_12,        //0-4095 output
+        .atten = ADC_ATTEN_DB_12,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, ADC_CHANNEL_2, &config));  //Setting up GPIO pin 2
+
+    while(true){
+        //Reading input from GPIO Pin 2
+        ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, ADC_CHANNEL_2, out_raw_GPIO2));
+
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
 void app_main(void)
 {
     // Replace with your actual Wi-Fi network name and password
@@ -66,8 +92,8 @@ void app_main(void)
     gpio_set_direction(BUZZER_PIN, GPIO_MODE_OUTPUT);
     gpio_set_direction(RED_LED_PIN, GPIO_MODE_OUTPUT);
     gpio_set_direction(GREEN_LED_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_INPUT);
-    gpio_set_direction(GPIO_NUM_12, GPIO_MODE_INPUT);
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_INPUT);        //Alcohol Sensor
+    gpio_set_direction(GPIO_NUM_12, GPIO_MODE_INPUT);       //Heart Pulse Sensor
 
     // Configure LEDC timer
     ledc_timer_config_t ledc_timer = {
@@ -99,6 +125,15 @@ void app_main(void)
     ledc_channel_config(&green_ledc_channel);
 
     //creating tasks that should run until power off/reset
-    xTaskCreate(redLightTask, "redLightTask function", 500, NULL, 1, NULL);
-    xTaskCreate(greenLightTask, "greenLightTask function", 500, NULL, 1, NULL);
+    //xTaskCreate(redLightTask, "redLightTask function", 500, NULL, 1, NULL);
+    //xTaskCreate(greenLightTask, "greenLightTask function", 500, NULL, 1, NULL);
+
+    //Creating task for Alcohol Sensor
+    int out_raw_GPIO2;
+    xTaskCreate(alcoholSensorTask, "alcoholSensorTask", 1024, &out_raw_GPIO2, 1, NULL);
+
+    while(true){
+        printf("The output from GPIO2 is: %d\n", out_raw_GPIO2);
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
 }
